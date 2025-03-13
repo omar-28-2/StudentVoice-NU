3500f9192ba11550f75583187057dfbb40ae233e
Omar <omarelhosany3@gmail.com>
2025-03-14 01:50:24 +0200

    Add SampleRepository and ISampleRepository for testing BaseRepository

---
+++ StudentVoice-NU.Application/Interfaces/Repositories/ISampleRepository.cs
@@ -0,0 +1,9 @@
+using StudentVoiceNU.Domain.Entities;
+
+namespace StudentVoiceNU.Application.Interfaces.Repositories
+{
+    public interface ISampleRepository : IBaseRepository<SampleEntity>
+    {
+        // Any extra methods for SampleEntity (if needed)
+    }
+}
