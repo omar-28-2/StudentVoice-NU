3d0fe6d36db625728a166e9787dee25a94a436f0
Omar <omarelhosany3@gmail.com>
2025-03-14 02:32:31 +0200

    Register IEmailService in DI

--- StudentVoice-NU.Infrastructure/InfrastructureServiceDI.cs
+++ StudentVoice-NU.Infrastructure/InfrastructureServiceDI.cs
@@ -2,6 +2,8 @@ using Microsoft.Extensions.DependencyInjection;
 using StudentVoiceNU.Application.Interfaces.Repositories;
 using StudentVoiceNU.Infrastructure.Repositories;
 using StudentVoiceNU.Infrastructure.Repositories.Common;
+using StudentVoiceNU.Application.Interfaces.Services;
+using StudentVoiceNU.Infrastructure.Services;
 
 namespace StudentVoiceNU.Infrastructure
 {
@@ -11,6 +13,7 @@ namespace StudentVoiceNU.Infrastructure
         {
             services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
             services.AddScoped<ISampleRepository, SampleRepository>();
+            services.AddScoped<IEmailService, EmailService>();
         }
     }
 }
