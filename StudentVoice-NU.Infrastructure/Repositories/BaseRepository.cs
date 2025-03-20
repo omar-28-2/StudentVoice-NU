a043e02bb25ee508551b5a7aaab26b88b057746b
Omar <omarelhosany3@gmail.com>
2025-03-20 20:47:06 +0200

    ensure that the return values cannot be null

--- StudentVoice-NU.Infrastructure/Repositories/BaseRepository.cs
+++ StudentVoice-NU.Infrastructure/Repositories/BaseRepository.cs
@@ -12,6 +12,10 @@ namespace StudentVoiceNU.Infrastructure.Repositories
 
         public async Task<T> Create(T entity)
         {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
             await _table.AddAsync(entity);
             await _context.SaveChangesAsync();
             return entity;
@@ -22,7 +26,7 @@ namespace StudentVoiceNU.Infrastructure.Repositories
             var  entity = await _table.FindAsync(id);
             if (entity == null)
             {
-                return entity;
+                throw new KeyNotFoundException($"Entity with ID {id} not found.");
             }
             _table.Remove(entity);
             await _context.SaveChangesAsync();
@@ -31,11 +35,21 @@ namespace StudentVoiceNU.Infrastructure.Repositories
 
         public async Task<T> GetbyId(int id)
         {
-            return await _table.FindAsync(id);
+            var entity = await _table.FindAsync(id);
+            if (entity == null)
+            {
+                throw new KeyNotFoundException($"Entity with ID {id} not found.");
+            }
+            return entity;
+            
         }
 
         public async Task<T> Update(T entity)
         {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
             _table.Update(entity);
             await _context.SaveChangesAsync();
             return entity;
