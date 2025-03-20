39be01fad388f647e9ed5a4b627c8d1fb5d496aa
Omar <omarelhosany3@gmail.com>
2025-03-20 21:16:32 +0200

    all undercontrol

--- StudentVoice-NU.API/Controllers/TestController.cs
+++ StudentVoice-NU.API/Controllers/TestController.cs
@@ -1,14 +1,10 @@
-using StudentVoiceNU.Application.Interfaces.Repositories;
-using StudentVoiceNU.Domain.DTOs;
-using StudentVoiceNU.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace StudentVoiceNU.API.Controllers
 {
-    [ApiController]
     [Route("api/[controller]")]
-
-    public abstract class TestController : ControllerBase
+    [ApiController]
+    public class TestController : ControllerBase
     {
         [HttpGet]
         public IActionResult GetTest()
--- StudentVoice-NU.API/Program.cs
+++ StudentVoice-NU.API/Program.cs
@@ -1,28 +1,21 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-
+builder.Services.AddControllers(); 
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(); 
 
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "StudentVoiceNU API v1");
+    });
 }
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
