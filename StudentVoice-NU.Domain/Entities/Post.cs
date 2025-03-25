fb18caa534bddcb115e6a4a37e068f69bace975a
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-03-25 23:34:18 +0200

    Modified post table

--- StudentVoice-NU.Domain/Entities/Post.cs
+++ StudentVoice-NU.Domain/Entities/Post.cs
@@ -3,6 +3,7 @@ public class Post : AuditableEntity
     public string Content { get; set; }
     public bool IsAnonymous { get; set; }
     public int UserId { get; set; }
+    public Forum Forum { get; set; }
     public List<Comment> Comments { get; set; } = new();
     public List<Vote>Votes{ get; set; } = new();
      public User User { get; set; }
