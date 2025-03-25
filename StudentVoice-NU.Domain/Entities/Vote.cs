7f61684ca98544acff0705a3bc00415c8d348c0f
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-03-25 23:00:32 +0200

    Adding vote table

---
+++ StudentVoice-NU.Domain/Entities/Vote.cs
@@ -0,0 +1,15 @@
+using StudentVoiceNU.Domain.Entities;
+
+public class Vote : BaseEntity
+{
+    public int PostId { get; set; }
+    public int UserId { get; set; }
+    public VoteType Type { get; set; }
+    public Post Post { get; set; }
+    public User User { get; set; }
+}
+public enum VoteType
+{
+    UpVote,
+    DownVote
+}
