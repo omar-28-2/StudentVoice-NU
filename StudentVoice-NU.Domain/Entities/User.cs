208d00031ad6c20bf21cd367a01a2f3d445ecd65
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-03-25 22:59:56 +0200

    adding user table

---
+++ StudentVoice-NU.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+public class User : AuditableEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Major { get; set; }
+    public int EnrollmentYear { get; set; }
+    public UserRole Role { get; set; }
+    public List<Post> Posts { get; set; }
+}
+public enum UserRole
+{
+    Admin,
+    Student,
+    Alumni
+}
