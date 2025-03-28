465acb25c1bb4bfff5125a8ebc98621f4987b959
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-03-28 02:34:08 +0200

    Editing DBContext

--- StudentVoice-NU.Infrastructure/Contexts/StudentVoiceDbContext.cs
+++ StudentVoice-NU.Infrastructure/Contexts/StudentVoiceDbContext.cs
@@ -1,5 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using StudentVoiceNU.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using StudentVoiceNU.Infrastructure.Contexts;
 
 namespace StudentVoiceNU.Infrastructure.Contexts
 {
@@ -9,6 +14,17 @@ namespace StudentVoiceNU.Infrastructure.Contexts
         {
             
         }
-        public DbSet<BaseEntity> BaseEntities { get; set; } 
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Event> Events { get; set; }   
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<CommentLike> CommentLikes { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+      
+    
     }
 }
