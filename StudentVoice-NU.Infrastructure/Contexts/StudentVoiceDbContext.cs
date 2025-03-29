f42cd2becb948ead17ab1bb2bbb466b3c13497cf
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-03-29 16:07:32 +0200

    Removing duplicate heading line

--- StudentVoice-NU.Infrastructure/Contexts/StudentVoiceDbContext.cs
+++ StudentVoice-NU.Infrastructure/Contexts/StudentVoiceDbContext.cs
@@ -1,10 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using StudentVoiceNU.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 using StudentVoiceNU.Infrastructure.Contexts;
+using StudentVoiceNU.Infrastructure.Contexts.EntityConfigurations; 
 
 namespace StudentVoiceNU.Infrastructure.Contexts
 {
@@ -24,7 +24,23 @@ namespace StudentVoiceNU.Infrastructure.Contexts
         public DbSet<CommentLike> CommentLikes { get; set; }
         public DbSet<Admin> Admins { get; set; }
 
-      
-    
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//         {
+//             if (!optionsBuilder.IsConfigured)
+//             {
+//                 optionsBuilder.UseSqlServer("Server=DESKTOP-91S1SOI;Database=StudentVoice;Trusted_Connection=True;TrustServerCertificate=True;");
+//         }
+// }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    base.OnModelCreating(modelBuilder);
+
+    // Apply configurations
+    modelBuilder.ApplyConfiguration(new UserConfiguration());
+    modelBuilder.ApplyConfiguration(new PostConfiguration());
+    modelBuilder.ApplyConfiguration(new CommentConfiguration());
+    modelBuilder.ApplyConfiguration(new ForumConfiguration());
+    modelBuilder.ApplyConfiguration(new VoteConfiguration());
+}
     }
 }
