ac9dadb160e10697ed0e497182a4181ec2989ed2
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-03 03:35:19 +0200

    adding optional and virtual

--- StudentVoice-NU.Domain/Entities/Forum.cs
+++ StudentVoice-NU.Domain/Entities/Forum.cs
@@ -1,10 +1,15 @@
 public class Forum : AuditableEntity
 {
-    public string   Name { get; set; }
-    public int ForumID { get; set; }
-    
+    public string Name { get; set; }
     public string Description { get; set; }
     public bool IsAnonymous { get; set; }
-    public string Type { get; set; }
-    public List<Post> Posts { get; set; }=new();
+    public ForumType Type { get; set; }
+    public virtual List<Post>? Posts { get; set; }=new();
+}
+public enum ForumType
+{
+    General,
+    Club,
+    Event,
+    Course
 }
