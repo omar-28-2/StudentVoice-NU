ec0c093417ef5dffccdd597593aa8c9e006e72c0
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-03 03:34:55 +0200

    adding optional and virtual

--- StudentVoice-NU.Domain/Entities/CommentLike.cs
+++ StudentVoice-NU.Domain/Entities/CommentLike.cs
@@ -3,6 +3,6 @@ public class CommentLike : BaseEntity
 {
     public int CommentId { get; set; }
     public int UserId { get; set; }
-    public Comment Comment { get; set; }
-    public User User { get; set; }
+    public Comment? Comment { get; set; }
+    public User? User { get; set; }
 }
