574ec360bfbdd993be1709983b420aa92044d638
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-03 03:34:46 +0200

    adding optional and virtual

--- StudentVoice-NU.Domain/Entities/Comment.cs
+++ StudentVoice-NU.Domain/Entities/Comment.cs
@@ -3,7 +3,7 @@ public class Comment : AuditableEntity
     public string Content { get; set; }
     public int PostId { get; set; }
     public int UserId { get; set; }
-    public Post Post { get; set; }
-    public User User { get; set; }
-    public List<CommentLike>CommentLikes{ get; set; }=new();
+    public Post? Post { get; set; }
+    public User? User { get; set; }
+    public virtual List<CommentLike>? CommentLikes{ get; set; }=new();
 }
