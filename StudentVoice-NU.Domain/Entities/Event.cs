65c121f956e47ce7dfa8fbeef8ac639624bf9492
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-11 16:16:26 +0200

    removing optional from name and make it auditable entity

--- StudentVoice-NU.Domain/Entities/Event.cs
+++ StudentVoice-NU.Domain/Entities/Event.cs
@@ -1,9 +1,9 @@
 using StudentVoiceNU.Domain.Entities;
 
-public class Event : BaseEntity
+public class Event : AuditableEntity
 {
+    public string Name { get; set; }
+    public string Description { get; set; }
     public int ClubId { get; set; }
-    public Club? Club { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
+    public virtual Club? Club { get; set; }
 }
