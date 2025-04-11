70892d9ef90b2fc079a6ff8f4e9d7de9089e09f0
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-11 16:17:53 +0200

    removing optional from crucial info

--- StudentVoice-NU.Domain/Entities/User.cs
+++ StudentVoice-NU.Domain/Entities/User.cs
@@ -1,9 +1,9 @@
 public class User : AuditableEntity
 {
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Email { get; set; }
-    public string? Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
     public int EnrollmentYear { get; set; }
     public UserRole Role { get; set; }
     public int MajorId { get; set; }
