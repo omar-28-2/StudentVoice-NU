fc8542e383adcfbf72f0a10cf1424e8e58317ea8
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-20 22:10:48 +0200

    Fixing bug

--- StudentVoice-NU.Domain/Entities/Club.cs
+++ StudentVoice-NU.Domain/Entities/Club.cs
@@ -1,8 +1,9 @@
 using StudentVoiceNU.Domain.Entities;
-
+using System.Text.Json.Serialization;
 public class Club: BaseEntity
 {
     public string Name{get;set;}
     public string Email{get;set;}
+    [JsonIgnore]
     public virtual List<Event>? Events{get;set;}=new();
 }
