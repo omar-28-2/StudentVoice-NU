b4b11bca6c27a177b96299a16dba1a7e5706542f
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-25 02:04:49 +0300

    Modyfing ipost repo

--- StudentVoice-NU.Application/Interfaces/Repositories/IPostRepository.cs
+++ StudentVoice-NU.Application/Interfaces/Repositories/IPostRepository.cs
@@ -2,6 +2,8 @@
 namespace StudentVoiceNU.Application.Interfaces.Repositories
 {
     public interface IPostRepository  : IBaseRepository<Post> {
-        Task<IEnumerable<Post>> GetPostsByUserId(int userId);
+        Task<IEnumerable<Post>> GetPostsByUserId(int userId, int postNumber, int postSize );
+        Task<Post?> GetbyId(int id);
+        
     }
 }
