17141b04a670126c91f19f22b51fa1ca7d051641
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-25 02:07:45 +0300

    Modyfing Post controller

--- StudentVoice-NU.API/Controllers/PostController.cs
+++ StudentVoice-NU.API/Controllers/PostController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using StudentVoiceNU.Application.Interfaces.Repositories;
 using StudentVoiceNU.Domain.Entities;
+using StudentVoiceNU.Infrastructure.Services;
+using StudentVoiceNU.Application.Services;
+using StudentVoiceNU.Domain.DTOs;
 
 namespace StudentVoiceNU.API.Controllers
 {
@@ -9,41 +12,50 @@ namespace StudentVoiceNU.API.Controllers
     public class PostController : ControllerBase
     {
         private readonly IPostRepository _postRepository;
-
+        private readonly IPostService _postService;
         public PostController(IPostRepository postRepository)
         {
             _postRepository = postRepository;
+            _postService = new PostService(postRepository);
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Post>> GetPost(int id)
-        {
-            var post = await _postRepository.GetbyId(id);
-            if (post == null) return NotFound();
-            return Ok(post);
-        }
+    [HttpGet("user/{userId}/posts")]
+    public async Task<IActionResult> GetPost(
+    int userId,
+    [FromQuery] int postNumber = 1,
+    [FromQuery] int postSize = 10)
+    {
+        var posts = await _postService.GetPostsByUser(userId, postNumber, postSize);
+        return Ok(posts);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetPostById(int id)
+    {
+        var post = await _postService.GetPostById(id);
+        if (post == null) return NotFound();
+        return Ok(post);
+    }
+    [HttpPost]
+    public async Task<ActionResult<Post>> CreatePost([FromBody] CreatePostDto createPostDto)
+    {
+            var createdPost = await _postService.CreatePost(createPostDto);
+            return CreatedAtAction(nameof(GetPostById), new { id = createdPost.Id }, createdPost);
+    }
 
-        [HttpPost]
-        public async Task<ActionResult<Post>> CreatePost(Post post)
-        {
-            var createdPost = await _postRepository.Create(post);
-            return CreatedAtAction(nameof(GetPost), new { id = createdPost.Id }, createdPost);
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdatePost(int id, [FromBody] CreatePostDto updatePostDto)
+    {
+        var isUpdated = await _postService.UpdatePost(id, updatePostDto);
+        if (!isUpdated) return NotFound();
+        return NoContent();
+    }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdatePost(int id, Post post)
-        {
-            if (id != post.Id) return BadRequest();
-            await _postRepository.Update(post);
-            return NoContent();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePost(int id)
-        {
-            var deletedPost = await _postRepository.Delete(id);
-            if (deletedPost == null) return NotFound();
-            return NoContent();
-        }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePost(int id)
+    {
+        var isDeleted = await _postService.DeletePost(id);
+        if (!isDeleted) return NotFound();
+        return NoContent();
+    }
     }
 }
