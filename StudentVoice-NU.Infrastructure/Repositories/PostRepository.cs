06c1beea652954941247ed69a40edabbe2bd3225
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-25 16:57:33 +0300

    adding pagination functions

--- StudentVoice-NU.Infrastructure/Repositories/PostRepository.cs
+++ StudentVoice-NU.Infrastructure/Repositories/PostRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using StudentVoiceNU.Application.Interfaces.Repositories;
+using StudentVoiceNU.Domain.DTOs;
 using StudentVoiceNU.Domain.Entities;
 using StudentVoiceNU.Infrastructure.Contexts;
 
@@ -13,23 +14,60 @@ namespace StudentVoiceNU.Infrastructure.Repositories
         {
             _context = context;
         }
-        public async Task<Post?> GetById(int id) 
+    public async Task<ReadPostDto?> GetPostbyId(int id,int commentsPageNumber = 1, int commentsPageSize = 10, int votesPageNumber = 1, int votesPageSize = 10)
+    {
+    return await _context.Posts
+        .Where(p => p.Id == id)
+        .Select(p => new ReadPostDto
         {
-            return await _context.Posts
-                .Include(p => p.Comments)
-                .Include(p => p.Votes)
-                .FirstOrDefaultAsync(p => p.Id == id);
+            Id = p.Id,
+            Content = p.Content,
+            CreatedAt = p.CreatedAt,
+            CommentsCount = p.Comments.Count,
+            VotesCount = p.Votes.Count
+        })
+        .FirstOrDefaultAsync();
         }
-        public async Task<IEnumerable<Post>> GetPostsByUserId(int userId,int postNumber=1,int postSize=10)
-        {
-            return await _context.Posts
-                .Where(p => p.UserId == userId)
-                .Include(p => p.Comments) 
-                .Include(p => p.Votes)    
-                .OrderByDescending(p => p.CreatedAt)
-                .Skip((postNumber - 1) * postSize)
-                .Take(postSize)
-                .ToListAsync();
-        }
+    public async Task <IEnumerable<Comment>> GetCommentsByPostId(int postId,int pageNumber = 1, int pageSize = 10)
+    {
+        return await _context.Comments
+        .Where(c => c.PostId == postId)
+        .OrderBy(c => c.CreatedAt)
+        .Skip((pageNumber - 1) * pageSize)
+        .Take(pageSize)
+        .ToListAsync();
+    }
+    public async Task<IEnumerable<Vote>> GetVotesByPostId(int postId,int pageNumber = 1, int pageSize = 10)
+    {
+        return await _context.Votes
+        .Where(v => v.PostId == postId)
+        .Skip((pageNumber - 1) * pageSize)
+        .Take(pageSize)
+        .ToListAsync();
+    }
+    public async Task<int> GetCommentsCountByPostId(int postId)
+    {
+        return await _context.Comments
+            .Where(c => c.PostId == postId)
+            .CountAsync();
+    }
+
+    public async Task<int> GetVotesCountByPostId(int postId)
+    {
+        return await _context.Votes
+            .Where(v => v.PostId == postId)
+            .CountAsync();
+    }
+    public async Task<IEnumerable<Post>> GetPostsByUserId(int userId,int postNumber=1,int postSize=10)
+    {
+        return await _context.Posts
+            .Where(p => p.UserId == userId)
+            .Include(p => p.Comments) 
+            .Include(p => p.Votes)    
+            .OrderByDescending(p => p.CreatedAt)
+            .Skip((postNumber - 1) * postSize)
+            .Take(postSize)
+            .ToListAsync();
+    }
     }
 }
