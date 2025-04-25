27d2ab2cb4926a096558729f6c6703eada91e387
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-25 16:53:13 +0300

    Adding comments and votes count and their data

--- StudentVoice-NU.Application/Interfaces/Repositories/IPostRepository.cs
+++ StudentVoice-NU.Application/Interfaces/Repositories/IPostRepository.cs
@@ -1,9 +1,14 @@
 
+using StudentVoiceNU.Domain.DTOs;
+
 namespace StudentVoiceNU.Application.Interfaces.Repositories
 {
     public interface IPostRepository  : IBaseRepository<Post> {
-        Task<IEnumerable<Post>> GetPostsByUserId(int userId, int postNumber, int postSize );
-        Task<Post?> GetbyId(int id);
-        
+        Task<IEnumerable<Post>> GetPostsByUserId(int userId, int pageNumber, int pageSize );
+        Task<ReadPostDto?> GetPostbyId(int id, int commentsPageNumber = 1, int commentsPageSize = 10, int votesPageNumber = 1, int votesPageSize = 10);
+        Task<IEnumerable<Comment>> GetCommentsByPostId(int postId, int pageNumber = 1, int pageSize = 10);
+        Task<IEnumerable<Vote>> GetVotesByPostId(int postId, int pageNumber = 1, int pageSize = 10);
+        Task<int> GetCommentsCountByPostId(int postId);
+        Task<int> GetVotesCountByPostId(int postId);
     }
 }
