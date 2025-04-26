788722f6fef843017789f31d6be549d70b0ca7ac
Rwan Ashraf <r.asharf2257@nu.edu.eg>
2025-04-26 16:42:50 +0300

    Removing counting functions

--- StudentVoice-NU.Application/Interfaces/Repositories/IPostRepository.cs
+++ StudentVoice-NU.Application/Interfaces/Repositories/IPostRepository.cs
@@ -8,7 +8,6 @@ namespace StudentVoiceNU.Application.Interfaces.Repositories
         Task<ReadPostDto?> GetPostbyId(int id, int commentsPageNumber = 1, int commentsPageSize = 10, int votesPageNumber = 1, int votesPageSize = 10);
         Task<IEnumerable<Comment>> GetCommentsByPostId(int postId, int pageNumber = 1, int pageSize = 10);
         Task<IEnumerable<Vote>> GetVotesByPostId(int postId, int pageNumber = 1, int pageSize = 10);
-        Task<int> GetCommentsCountByPostId(int postId);
-        Task<int> GetVotesCountByPostId(int postId);
+        
     }
 }
